4c3dbf0acb21947e9152cb12f0697d6b4ef633d1
Luke Russell <thelukerussel21@gmail.com>
2020-06-11 14:58:56 -0400

    app.js update

--- App.js
+++ App.js
@@ -52,10 +52,10 @@ bikeListFlow.navigationOptions = {
 };
 
 const switchNavigator = createSwitchNavigator({
-  // ResolveAuth: ResolveAuthScreen,
-  // loginFlow: createStackNavigator({
-  //   Signin: SigninScreen,
-  // }),
+  ResolveAuth: ResolveAuthScreen,
+  loginFlow: createStackNavigator({
+    Signin: SigninScreen,
+  }),
   mainFlow: createBottomTabNavigator(
     {
       bikeListFlow,
